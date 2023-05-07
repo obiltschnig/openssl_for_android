7fb9871a1904883c5f071fa67a0c3899bfeb154a
Günter Obiltschnig <guenter.obiltschnig@appinf.com>
2023-05-07 19:01:24 +0200

    cleanup

--- openssl_build.sh
+++ openssl_build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash -e
 
+OPENSSL_VER=${OPENSSL_VERSION:-3.0.8}
 WORK_PATH=$(cd "$(dirname "$0")";pwd)
 ANDROID_NDK_PATH=${WORK_PATH}/android-ndk-r20b
-OPENSSL_SOURCES_PATH=${WORK_PATH}/openssl-3.0.8
+OPENSSL_SOURCES_PATH=${WORK_PATH}/openssl-${OPENSSL_VER}
 ANDROID_TARGET_API=$1
 ANDROID_TARGET_ABI=$2
-OUTPUT_PATH=${WORK_PATH}/openssl_3.0.8_${ANDROID_TARGET_ABI}
+OUTPUT_PATH=${WORK_PATH}/openssl_${OPENSSL_VER}_${ANDROID_TARGET_ABI}
 
 OPENSSL_TMP_FOLDER=/tmp/openssl_${ANDROID_TARGET_ABI}
 mkdir -p ${OPENSSL_TMP_FOLDER}
@@ -46,22 +47,6 @@ then
     PATH=$ANDROID_NDK_ROOT/toolchains/llvm/prebuilt/linux-x86_64/bin:$ANDROID_NDK_ROOT/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin:$ANDROID_NDK_ROOT/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin:$PATH
     cd ${OPENSSL_TMP_FOLDER}
     ./Configure android-arm64 -D__ANDROID_API__=${ANDROID_TARGET_API} -static no-shared no-tests --prefix=${OUTPUT_PATH} -fPIC
-    build_library
-
-elif [ "$ANDROID_TARGET_ABI" == "mips" ]
-then
-    export ANDROID_NDK_ROOT=${ANDROID_NDK_PATH}
-    PATH=$ANDROID_NDK_ROOT/toolchains/llvm/prebuilt/linux-x86_64/bin:$ANDROID_NDK_ROOT/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin:$ANDROID_NDK_ROOT/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin:$PATH
-    cd ${OPENSSL_TMP_FOLDER}
-    ./Configure android-mips -D__ANDROID_API__=${ANDROID_TARGET_API} -static no-shared no-tests --prefix=${OUTPUT_PATH} -fPIC
-    build_library
-
-elif [ "$ANDROID_TARGET_ABI" == "mips64" ]
-then
-    export ANDROID_NDK_ROOT=${ANDROID_NDK_PATH}
-    PATH=$ANDROID_NDK_ROOT/toolchains/llvm/prebuilt/linux-x86_64/bin:$ANDROID_NDK_ROOT/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin:$ANDROID_NDK_ROOT/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin:$PATH
-    cd ${OPENSSL_TMP_FOLDER}
-    ./Configure android-mips64 -D__ANDROID_API__=${ANDROID_TARGET_API} -static no-shared no-tests --prefix=${OUTPUT_PATH} -fPIC
     build_library
 
 elif [ "$ANDROID_TARGET_ABI" == "x86" ]
